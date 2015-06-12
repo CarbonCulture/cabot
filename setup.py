b3970d88fc62f314037c7ef9db48120ef7703dea
Chris Elsmore <chris.elsmore@carbonculture.net>
2015-06-12 14:57:36 +0100

    dependency-link typo

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email='info@arachnys.com',
     url='http://cabotapp.com',
     license='MIT',
-    dependency_links = ['http://github.com/CarbonCulture/cabot-alert-slack/tarball/master#egg=cabot_alert_slack==0.5.2'],
+    dependency_links = ['http://github.com/CarbonCulture/cabot-alert-slack/tarball/master#egg=cabot_alert_slack-0.5.2'],
     install_requires=[
         'Django==1.6.8',
         'Markdown==2.5',
